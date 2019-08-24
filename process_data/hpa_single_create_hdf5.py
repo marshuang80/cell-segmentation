5d39c57cf38042742d77237d7f0397b568039bf1
marshuang80 <marshuang80@gmail.com>
2019-08-24 13:48:13 -0700

    new data processing scipts for neuro and single hpa data

---
+++ process_data/hpa_single_create_hdf5.py
@@ -0,0 +1,66 @@
+import os
+import argparse
+import glob
+
+import h5py
+import tqdm
+import imageio
+import numpy as np
+
+
+def load_ids(input_dir):
+    red_filenames = glob.glob(input_dir + '/*_red.png')
+    green_filenames = glob.glob(input_dir + '/*_green.png')
+    blue_filenames = glob.glob(input_dir + '/*_blue.png')
+    yellow_filenames = glob.glob(input_dir + '/*_yellow.png')
+
+    return [filename[:-8] for filename in red_filenames]
+
+
+def process(id_lists, output_dir):
+    
+    hdf5_fn = h5py.File(os.path.join(output_dir, 'data.hdf5'), "a")
+
+    for id_str in tqdm.tqdm(id_lists):
+
+        output_filename = os.path.join(output_dir, os.path.basename(id_str) + '.png')
+        if os.path.exists(output_filename):
+            continue
+
+        red_filename = id_str + '_red.png'
+        green_filename = id_str + '_green.png'
+        blue_filename = id_str + '_blue.png'
+        yellow_filename = id_str + '_yellow.png'
+
+        if not os.path.exists(red_filename) or \
+           not os.path.exists(green_filename) or \
+           not os.path.exists(blue_filename) or \
+           not os.path.exists(yellow_filename):
+            continue
+
+        for filename in [red_filename, blue_filename, yellow_filename]:
+            data = imageio.imread(filename)
+            name = filename.split("/")[-1]
+            hdf5_fn.create_dataset(str(name), data=data, dtype=np.uint8, chunks=True)
+
+    hdf5_fn.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', dest='input_dir',
+                        help='the directory of the input images',
+                        default=None, type=str)
+    parser.add_argument('--output_dir', dest='output_dir',
+                        help='the directory of the output images',
+                        default=None, type=str)
+    args = parser.parse_args()
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    id_lists = load_ids(args.input_dir)
+    process(id_lists, args.output_dir)
+
+
+if __name__ == '__main__':
+    main()
---
+++ process_data/neuro_create_hdf5.py
@@ -0,0 +1,56 @@
+import glob
+import json
+import imageio
+import h5py
+import numpy as np
+import os
+import argparse
+
+
+def main(args):
+    files = glob.glob(os.path.join(args.input_dir, "*"))
+    file_names = [file.split("_")[1].split(".")[0] for file in files if "image" in file]
+
+    #for name in file_names:
+    for name in file_names:
+        hdf5 = h5py.File((args.output_file), "a")
+
+        file_path = f"./train/image_{name}.tif"
+        mask_path = f"./train/mask_{name}.tif"
+        regions_path = f"./train-regions/regions_{name}.json"
+        
+        img = imageio.imread(file_path)
+        # might need average
+        img = img[:,:,0]
+        mask = imageio.imread(mask_path)
+        mask[mask > 0] = 1
+
+        regions = json.load(open(regions_path))
+        volume = [img, img, img, mask]
+        
+        for region in regions['regions']:
+            coords = np.array(region['coordinates'])
+
+            instance = np.zeros_like(img)
+            instance[[coords[:,0],coords[:,1]]] = 1
+            
+            volume.append(instance)
+            
+        img_stack = np.stack(volume)
+        
+        hdf5_train.create_dataset(str(name), data=img_stack, dtype=np.int, chunks=True)
+
+    hdf5.close()
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input_dir", type=str)
+    parser.add_argument("--output_file", type=str)
+
+    args = parser.parse_args()
+
+    main(args)
+
