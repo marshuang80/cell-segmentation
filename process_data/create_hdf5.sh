4c53116e9882bd7edd0cb263c2eefcc16204ffa6
marshuang80 <marshuang80@gmail.com>
2019-08-24 16:11:43 -0700

    update example script for creating hdf5

--- process_data/create_hdf5.sh
+++ process_data/create_hdf5.sh
@@ -1,4 +1,4 @@
-python create_hdf5.py --input_dir "/home/mars/data/stage1_train" \
-                      --output_dir "/home/mars/data/"
+python create_hdf5.py --input_dir "PATH_TO_TRAINING_DATA" \
+                      --output_dir "PATH_TO_OUTPUT_DIR"
 
 
